9f4f196a9b006d0fe3ed34b9246ff1681ae8d6b8
vanaja623 <perumalla554@gmail.com>
2015-04-20 16:27:04 +0530

    1st cmt

---
+++ sample/src/main/java/sample/addition.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class addition {
+
+	public static void main(String[] args) {
+		
+		 String str = "Hello World";
+	      String anotherString = "hello world";
+	      Object objStr = str;
+
+	      System.out.println( str.compareTo(anotherString) );
+	      System.out.println( str.compareToIgnoreCase(anotherString) );
+	      System.out.println( str.compareTo(objStr.toString()));
+
+	}
+
+}
